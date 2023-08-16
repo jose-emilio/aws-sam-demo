07f448fc2beb9d3de32103be988c76de0d73e4a0
José E. Vera <36360639+jose-emilio@users.noreply.github.com>
2023-08-16 16:22:07 +0200

    Add files via upload

---
+++ lambda/aplicacion.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+import json
+
+def manejador(event, context):
+    for record in event['Records']:
+        print('IDEvento: ' + record['eventID'])
+        print('NombreEvento: ' + record['eventName'])
+        print("Registro de DynamoDB:\n " + json.dumps(record['dynamodb'], indent=2))
+    print('Se han procesado %s registros.' % str(len(event['Records'])))
+    return {"Registros":str(len(event['Records']))}
